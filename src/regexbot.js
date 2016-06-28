17f0891176265edb8a0b187acf64d4a467114e9b
Steve Melia <steve.j.melia@gmail.com>
2016-06-28 15:41:17 +0100

    Fixed bug handling multiple matches (/g) and stateful regex

--- src/regexbot.js
+++ src/regexbot.js
@@ -3,16 +3,21 @@ var regexbot = {};
 
 regexbot.respond = function (text) {
   for (var item of regexbot.config.regexes) {
-    var match = item.regex.exec(text);
-    if (!match) {
-      continue;
+    var match;
+    var output = '';
+    while ((match = item.regex.exec(text)) !== null) {
+      if (output.length > 0) output += '\n';
+
+      var msg = item.message;
+      for (var i = 0; i < match.length; i++) {
+        msg = msg.replace('[' + i + ']', match[i]);
+      }
+      output += msg;
     }
 
-    var msg = item.message;
-    for (var i = 0; i < match.length; i++) {
-      msg = msg.replace('[' + i + ']', match[i]);
+    if (output.length > 0) {
+      return output;
     }
-    return msg;
   }
   return null;
 };
