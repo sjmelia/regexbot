0dcca73230b0a539c88c68adc54bc2788648182d
Steve Melia <steve.j.melia@gmail.com>
2016-06-29 21:45:54 +0100

    Added test for groups

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ var assert = require('chai').assert;
 var regexbot = require('../src/regexbot');
 var config = {
   regexes: [
-    { regex: /[A-Z]+-[0-9]+/g, message: 'http://my-jira.com/?q=[0]' }
+    { regex: /[A-Z]+-[0-9]+/g, message: 'http://my-jira.com/?q=[0]' },
+    { regex: /([A-Z]+)-([A-Z]+)/g, message: 'A:[0] B:[1] C:[2]' }
   ]
 };
 regexbot.config = config;
@@ -15,8 +16,13 @@ describe('Regexbot', function () {
     assert.equal(reply, 'http://my-jira.com/?q=CL-100');
   });
 
-  it('should return multiples seperated by newlines', function() {
+  it('should return multiples seperated by newlines', function () {
     var reply = regexbot.respond('CL-100 CL-200');
     assert.equal(reply, 'http://my-jira.com/?q=CL-100\nhttp://my-jira.com/?q=CL-200');
+  });
+
+  it('should populate with groups', function () {
+    var reply = regexbot.respond('ABC-DEF');
+    assert.equal(reply, 'A:ABC-DEF B:ABC C:DEF');
   });
 });
