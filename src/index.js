aad6721fcffedf9933c9e166d5b3e093e9020181
Bastian Schaar <bassch@users.noreply.github.com>
2017-01-11 18:25:05 +0100

    Fix incorrect template literals
    * fix incorrect template literals usage

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log('Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel');
+  console.log(`Logged in as "${rtmStartData.self.name}" of team "${rtmStartData.team.name}", but not yet connected to a channel`);
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
