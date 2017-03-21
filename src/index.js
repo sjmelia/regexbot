02204572637a548743d6f61d7f945a3007c916eb
Steve Melia <steve.j.melia@gmail.com>
2017-03-21 08:22:54 +0000

    Added schedules, rabbit, and updated config example

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ var RtmClient = slackClient.RtmClient;
 var WebClient = slackClient.WebClient;
 
 var rtm = new RtmClient(config.slack_api_token);
-rtm.start();
+var startInfo = rtm.start();
 
 var web = new WebClient(config.slack_api_token);
 
@@ -20,6 +20,9 @@ var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
     console.log(`Logged in as "${rtmStartData.self.name}" of team "${rtmStartData.team.name}", but not yet connected to a channel`);
+	
+	console.log(rtmStartData.self.id);
+	config.build(rtmStartData.self.id);
     });
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
--- src/schedule.js
+++ src/schedule.js
@@ -3,7 +3,8 @@ var schedule = require('node-schedule');
 
 module.exports = function (schedules, poster) {
 	var jobs = [];
-	for (var item of schedules) {
+	for (var unclosedItem of schedules) {
+		let item = unclosedItem;
 		console.log(`Registering a job with schedule ${item.cron} for channel ${item.channel} and message ${item.message}`);
 		var job = schedule.scheduleJob(item.cron, function() { poster(item.channel, item.message); });
 		jobs.push(job);
