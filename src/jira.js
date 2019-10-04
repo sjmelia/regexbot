4b8bf66aba09521a9588ef38a94866ff127e455f
Stephen Melia <stephen.melia@nhs.net>
2019-10-04 10:45:14 +0100

    Added fn to find jira tickets based on exception alerts logged into splunk

--- src/jira.js
+++ src/jira.js
@@ -21,12 +21,48 @@ function responder (jira, config) {
   };
 }
 
+function finder (jira, config) {
+  return function (match, callback) {
+    var issueId = match[1];
+	issueId = issueId.replace(/\+|\.|\,|\;|\?|\*|\/|\%|\^|\$|\#|\@|\[|\]/g, " ");
+    var query = "text ~ \"" + issueId + "\"";
+	console.log("Searching with query " + query);
+    jira.searchJira(query)
+      .then(function (results) {
+		if (results.total > 0)
+		{
+			var issue = results.issues[0];
+			var link = config.protocol + '://' + config.host + ':' + config.port + '/browse/' + issue.key;
+			var slackLink = '<' + link + '|' + issue.key + '>';
+			callback("This exception could be " + slackLink + ': ' + issue.fields.summary);
+		}			
+      })
+    .catch(function (err) {
+      //if (err.statusCode === 404) {
+//        callback(slackLink + ' - I couldn\'t find this one... (404)');
+  //    } else {
+        console.error(err);
+    //    callback(slackLink + ' - Oh no! An error occured (' + err.statusCode + ')');
+      //}
+    });
+  };
+}
+
+
 function JiraResponder (config) {
-  var jira = new JiraApi(config);
+	var jira = new JiraApi(config);
   return {
     regex: /(^|\s)([A-Za-z]+-[0-9]+)/g,
     message: responder(jira, config)
   };
 }
 
-module.exports = JiraResponder;
+function JiraFinder (config) {
+	var jira = new JiraApi(config);
+	return {
+		regex: /One or more exceptions have occurred - first message is (.*) exception is (.*)/g,
+		message: finder(jira, config)
+	};
+}
+
+module.exports = { JiraResponder, JiraFinder };
--- src/schedule.js
+++ src/schedule.js
@@ -1,8 +1,53 @@
 /* vim: set ft=javascript ts=2 et sw=2 tw=80: */
 var schedule = require('node-schedule');
+var http = require('http');
 
 module.exports = function (schedules, poster) {
   var jobs = [];
+
+	var getRota =	 function() {
+		var today = new Date();
+		var year = today.getUTCFullYear();
+		var month = ('0' + (today.getUTCMonth() + 1)).slice(-2);
+		var day = ('0' + today.getUTCDate()).slice(-2);
+		
+		 var options = {
+			  host: 'www.rotatastic.com',
+			  path: '/rota/af59070d-b2c4-49c5-a425-4ed1ab27448b/entries/' + year + '/' + month,
+			  method: 'GET'
+			};
+
+		http.get(options, function(res) {
+		  var body = '';
+		  res.on('data', function(chunk) {
+			body += chunk;
+		  });
+		  res.on('end', function() {
+			  var msg = JSON.parse(body);
+			  var key = year + '-' + month + '-' + day;
+			  //console.log(msg);
+			  console.log(key);
+			  var onRota = msg.find(function(ele) { return ele['date'] === key; });
+			  
+			  //console.log(body);
+			console.log(onRota.entry);
+			var entry = onRota.entry;
+			if (entry === '')
+			{
+				entry = "No one!";
+			}
+			
+			poster('#sdcs', 'On the <http://www.rotatastic.com/#/af59070d-b2c4-49c5-a425-4ed1ab27448b/|rota> today: ' + entry);
+		  });
+		}).on('error', function(e) {
+		  console.log("Got error: " + e.message);
+		}); 
+	};
+	//getRota();
+	//var job = schedule.scheduleJob("0 9 * * *", getRota);
+
+	//jobs.push(job);
+
   for (var unclosedItem of schedules) {
     let item = unclosedItem;
     console.log(`Registering a job with schedule ${item.cron} for channel ${item.channel} and message ${item.message}`);
